d15a50606395d8670d1072e688691ae2a920601f
Jesse Dyer <dyerjdeveloper@gmail.com>
2021-09-27 15:54:28 -0600

    update 9/27

--- footystats/players_parse.py/players_parse.py
+++ footystats/players_parse.py/players_parse.py
@@ -5,12 +5,31 @@ import json
 response = requests.get("https://api.football-data-api.com/league-players?key=749c030e9b03727c95239d2eafaac41efafa185f35996ac07591bbbbc2f9e47d&season_id=6135")
 data = response.json()
 
-count = 0 
-for row in data['data']:
-	count += 1
-	print(count)
-	print(row)
 
 lst = []
 for row in data['data']:
-	
+			lst.append(row)
+
+for items in lst:
+	del items['id']
+	del items['competition_id']
+	del items['first_name']
+	del items['last_name']
+	del items['known_as']
+	del items['shorthand']
+	del items['league']
+	del items['league_type']
+	del items['starting_year']
+	del items['ending_year']
+	del items['url']
+	del items['club_team_2_id']
+	del items['national_team_id']
+	del items['birthday']
+	del items['continent']
+	del items['last_match_timestamp']
+
+print(lst)
+
+with open('playerstats.json', 'w', encoding='utf-8') as player_data:
+    json.dump(lst, player_data, ensure_ascii=False, indent=4)
+    print("Writing file to playerstats.json...")
